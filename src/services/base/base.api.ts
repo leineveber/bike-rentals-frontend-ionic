14722889cd61b82475467d256271d30388971a8e
leineveber <egor.leineveber@altence.com>
2022-12-04 12:52:15 +0300

    feat(api): use prod api

--- src/services/base/base.api.ts
+++ src/services/base/base.api.ts
@@ -4,7 +4,7 @@ import { QueryKeysEnum } from "../../common/models/QueryKeysEnum";
 import { StorageKeysEnum } from "../../common/models/StorageKeysEnum";
 import { queryClient } from "../../common/query-client/QueryClient";
 
-const BASE_URL = "http://192.168.0.102:4444";
+const BASE_URL = "https://bike-rentals-backend.glitch.me";
 
 export const axiosInstance = axios.create({ baseURL: BASE_URL });
 
